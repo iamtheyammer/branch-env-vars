68fc00ab3ae19dabcd937bac4bd7610ad71c3aac
iamtheyammer <iamtheyammer@gmail.com>
2020-07-13 13:47:54 -0700

    Actually don't access vars before initialization

--- src/index.js
+++ src/index.js
@@ -110,22 +110,24 @@ try {
   const ref = process.env.GITHUB_REF;
   const branchName = parseBranchName(ref);
 
-  const vars = vars.forEach(([name, possibleValues]) => {
-    if (!canOverwrite && !!process.env[name]) {
-      return;
-    }
-
-    const value = possibleValues[branchName] || possibleValues["!default"];
-    if (!value) {
-      if (setEmptyVars) {
-        core.exportVariable(name, "");
-        core.debug(`Exporting ${name} with an empty value`);
+  const vars = parseEnvVarPossibilities(process.env).forEach(
+    ([name, possibleValues]) => {
+      if (!canOverwrite && !!process.env[name]) {
+        return;
       }
-    } else {
-      core.exportVariable(name, value);
-      core.debug(`Exporting ${name} with value ${value}`);
+
+      const value = possibleValues[branchName] || possibleValues["!default"];
+      if (!value) {
+        if (setEmptyVars) {
+          core.exportVariable(name, "");
+          core.debug(`Exporting ${name} with an empty value`);
+        }
+      } else {
+        core.exportVariable(name, value);
+        core.debug(`Exporting ${name} with value ${value}`);
+      }
     }
-  });
+  );
 } catch (e) {
   core.setFailed(e);
 }
